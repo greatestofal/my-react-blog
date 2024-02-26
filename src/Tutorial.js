87828c79ba45c3edf1a563f6977694d5fe7ebb73
Adibe Nnamdi <adibennamdisolomon@gmail.com>
2024-02-26 14:45:18 +0100

    My first react blog

---
+++ src/Tutorial.js
@@ -0,0 +1,34 @@
+// import { useState } from "react";
+// import React from "react";
+
+// function Tutorial() {
+
+//     onst Home = () => {
+//         //   let name = "Nnamdi";
+//         const [name, setName] = useState("Nnamdi");
+
+//         const handleClick = (e) => {
+//           setName("Adibe");
+//           // name = "Adibe";
+//         };
+
+//         //   const handleClickAgain = (name, e) => {
+//         //     console.log("Hello" + name, e.target);
+//         //   };
+
+//         return (
+//           <div className="home">
+//             <h2> Home Page</h2>
+//             <p> {name} </p>
+//             <button onClick={handleClick}> Click Me</button>
+
+//             {/* <button onClick={(e) => handleClickAgain(" Nnamdi", e)}>
+//               Click me again
+//             </button> */}
+//           </div>
+//         );
+//       };
+
+// }
+
+// export default Tutorial;
